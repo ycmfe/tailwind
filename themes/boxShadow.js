0a1b62cc2e5af7293141e53eb51024c4466505d6
zhiqiang <zhiqiang0x0@outlook.com>
2020-11-02 16:37:51 +0800

    style: 修改配置增加 boxShadow & opacity

---
+++ themes/boxShadow.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+  return {
+    boxShadow: {
+      none: 'none',
+      sm: '0px 4px 6px 0px rgba(0, 0, 0, 0.06)',
+      md: '0px 8px 24px 0px rgba(0, 0, 0, 0.04)',
+      lg: '0px 24px 40px 0px rgba(0, 0, 0, 0.04)',
+    },
+  }
+}
--- themes/index.js
+++ themes/index.js
@@ -2,10 +2,14 @@ const colors = require('./colors')
 const screens = require('./screens')
 const spacing = require('./spacing')
 const borderRadius = require('./borderRadius')
+const boxShadow = require('./boxShadow')
+const opacity = require('./opacity')
 
 module.exports = {
   ...colors(),
   ...screens(),
   ...spacing(),
   ...borderRadius(),
+  ...boxShadow(),
+  ...opacity(),
 }
