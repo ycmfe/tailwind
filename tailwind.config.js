c86bfca8a1913218fcc347e5eaaad28455892ca2
zhiqiang <zhiqiang0x0@outlook.com>
2020-11-02 16:48:45 +0800

    feat: 增加 purge 配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 const theme = require('./themes')
+const isProd = process.env.production === 'production'
 
 module.exports = {
+  purge: {
+    enabled: isProd,
+    content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.ts'],
+  },
   future: {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
